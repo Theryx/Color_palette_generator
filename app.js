3bdbe754a65fe511a848305c3ba85144ae195e6b
Africa <ndouken@gmail.com>
2021-02-03 01:49:15 +0100

    Updating the BG

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const sliders = document.querySelectorAll(`input[type = "range"]`);
 const currentHexes = document.querySelectorAll(".color h2");
 let initialColors;
 
+//Event listeners
+sliders.forEach((slider) => {
+  slider.addEventListener("input", hslControls);
+});
+
 //Functions
 //color Generator
 function generateHex() {
@@ -56,5 +61,29 @@ function colorizeSlider(color, hue, brightness, saturation) {
     0
   )},${scaleBright(0.5)},${scaleBright(1)})`;
   hue.style.backgroundImage = `linear-gradient(to right, #FF0000, #FFFF00, #00FF00, #00FFFF, #0000FF, #FF00FF, #FF0000)`;
+}
+
+function hslControls(e) {
+  const index =
+    e.target.getAttribute("data-bright") ||
+    e.target.getAttribute("data-sat") ||
+    e.target.getAttribute("data-hue");
+
+  let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+  const hue = sliders[0];
+  const brightness = sliders[1];
+  const saturation = sliders[2];
+
+  const bgColor = initialColors[index];
+
+  let color = chroma(bgColor)
+    .set("hsl.s", saturation.value)
+    .set("hsl.l", brightness.value)
+    .set("hsl.h", hue.value);
+
+  colorDivs[index].style.backgroundColor = color;
+
+  //Colorize inputs/sliders
+  colorizeSliders(color, hue, brightness, saturation);
 }
 randomColors();
